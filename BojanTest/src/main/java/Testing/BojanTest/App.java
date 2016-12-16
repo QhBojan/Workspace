83076b0cf602feb2b4353e14f9f8eb51ada5f415
Qh Bojan <bdjakovic@qualityhouse.biz>
2016-12-16 16:51:01 +0100

    v 0.1

---
+++ BojanTest/src/main/java/Testing/BojanTest/App.java
@@ -0,0 +1,13 @@
+package Testing.BojanTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
